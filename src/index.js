730aaaefea0ec8d1e8b4afe98d820e62037ed3da
PIMPIYAJIRANAN <pim_kiko@hotmail.com>
2021-01-14 00:31:18 +0700

    firebase

--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import rootReducer from './store//reducer/rootReducer'
 import {Provider}  from 'react-redux'
 import thunk from 'redux-thunk'   //middleware
+import {reduxFirestore,getFirestore} from 'redux-firestore'
+import {reactReduxFirebase,getFirebase} from 'react-redux-firebase'
+import firebaseConfig from './config/firebaseConfig'
+import firebase from 'firebase/app'
 
-const store= createStore(rootReducer,applyMiddleware(thunk));
+// const store= createStore(rootReducer,
+// compose(
+//   applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
+//   reduxFirestore(firebaseConfig),
+//   reactReduxFirebase(firebaseConfig)
+//   )
+// );
+const store= createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirestore,getFirebase})),
+    reduxFirestore(firebase,firebaseConfig)
+    )
+);
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
