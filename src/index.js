ba9c7445657b80c4a6b324ede9d79df16ded712a
PIMPIYAJIRANAN <pim_kiko@hotmail.com>
2021-01-14 02:02:50 +0700

    completed firebase

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import {createStore,applyMiddleware,compose} from 'redux';
 import rootReducer from './store//reducer/rootReducer'
 import {Provider}  from 'react-redux'
 import thunk from 'redux-thunk'   //middleware
-import {reduxFirestore,getFirestore} from 'redux-firestore'
-import {reactReduxFirebase,getFirebase} from 'react-redux-firebase'
+import {reduxFirestore,getFirestore,createFirestoreInstance} from 'redux-firestore'
+import {reactReduxFirebase,getFirebase, ReactReduxFirebaseProvider} from 'react-redux-firebase'
 import firebaseConfig from './config/firebaseConfig'
 import firebase from 'firebase/app'
 
@@ -26,13 +26,22 @@ const store= createStore(rootReducer,
     )
 );
 
+const rrfProps = {
+  firebase,
+  config: firebaseConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
+
 
 
 
 ReactDOM.render(
   <React.StrictMode>
   <Provider store={store}>
+  <ReactReduxFirebaseProvider {...rrfProps}>
     <App />
+  </ReactReduxFirebaseProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
