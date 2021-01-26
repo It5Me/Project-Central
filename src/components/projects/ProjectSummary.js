ac2ecce627eb19ef0093634b0bf5a28ec296ca2e
It5Me <pim_kiko@hotmail.com>
2021-01-26 22:04:50 +0700

    color

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -10,7 +10,7 @@ const ProjectSummary = ({project}) =>{
             <div className="card z-depth-0 project-summary  add">
             <span className="card-title">{project.title}</span>
             <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-            <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
+            <p className=" textdateprojectsummary">{moment(project.createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     )
